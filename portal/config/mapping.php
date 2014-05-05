6aeeb1076843309c9424144b04e988ece3c0e484
peng.shen <peng.shen@indochino.com>
2014-05-05 09:53:28 -0700

    include paths

--- portal/config/mapping.php
+++ portal/config/mapping.php
@@ -1,13 +1,3 @@
 <?php
-register('/login',            new LoginController());
-register('/register',         new RegisterController());
-register('/forget', 		  new ForgetPasswordController());
-register('/logout',           new LogoutController());
-register('/profile',          new ProfileDetailController());
-register('/activation',       new ActivationController());
-register('/reset-password',   new ResetPasswordController());
-register('/pending', 		  new PendingController());
-register('/reset-email', 	  new ForgetEmailController());
-
-register('/external/profile.js', new ExternalHeaderController());
+register('/login',            new AdminLoginController());
 ?>
